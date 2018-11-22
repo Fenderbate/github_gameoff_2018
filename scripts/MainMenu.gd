c7c12bdc09f5487a49bd8f4062cc2d8b930951d7
Fenderbate <kfmfgi@gmail.com>
2018-11-22 23:09:58 +0100

    Added new stuff
    Added the following to the main menu:
    - Start  button
    - Options button
    - Exit button
    - Sound and Music volume sliders
    
    NOTE: The main manu has no complete controller support yet.

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,13 +1,117 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(PackedScene) var target_scene
 
-# Called when the node enters the scene tree for the first time.
+var pointer_pos = 0 setget pointer_pos_changed
+var pointer_pivot = Vector2()
+
+var are_options_on_screen = false
+
 func _ready():
-	pass # Replace with function body.
+	pointer_pivot = Vector2($Main/Pointer.rect_size.x/2,$Main/Pointer.rect_size.y/2)
+	$Main/Pointer.rect_global_position = $Main/PlayButton/PointerPos.global_position - pointer_pivot
+	if OS.get_name() == "HTML":
+		$Main/ExitButton.hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input(event):
+	
+	if Input.is_action_just_pressed("player_1_down"):
+		self.pointer_pos+=1
+	if Input.is_action_just_pressed("player_1_up"):
+		self.pointer_pos-=1
+	if Input.is_action_just_pressed("player_1_shoot"):
+		enter_menu(pointer_pos)
+	
+	if event is InputEventMouseButton:
+		if event.button_index == 1 and !event.pressed:
+			if $Options/Music.has_focus():
+				print("asd")
+				$Options/Music.release_focus()
+				$Music.stream_paused = true
+			if $Options/Sound.has_focus():
+				$Options/Music.release_focus()
+				$Sound.play()
+
+func enter_menu(index):
+	
+	match index:
+		0:
+			_on_PlayButton_button_down()
+		1:
+			_on_OptionsButton_button_down()
+		2:
+			_on_ExitButton_button_down()
+		_:
+			print("Menu unindentified: ",index)
+	
+
+
+	
+
+func pointer_pos_changed(value):
+	
+	if value > 2 or value < 0:
+			return
+	
+	pointer_pos = value
+	
+	match value:
+		0:
+			$Main/Pointer.rect_global_position = $Main/PlayButton/PointerPos.global_position - pointer_pivot
+		1:
+			$Main/Pointer.rect_global_position = $Main/OptionsButton/PointerPos.global_position - pointer_pivot
+		2:
+			if OS.get_name() != "HTML":
+				$Main/Pointer.rect_global_position = $Main/ExitButton/PointerPos.global_position - pointer_pivot
+				return
+			pointer_pos = 1
+		_:
+			print("something vent wrong...",value)
+		
+
+
+func _on_PlayButton_button_down():
+	Global.change_scene(target_scene)
+
+
+func _on_OptionsButton_button_down():
+	
+	if $AnimationPlayer.is_playing():
+		return
+	
+	match are_options_on_screen:
+		true:
+			$AnimationPlayer.play("optionsOut")
+			are_options_on_screen = false
+			return
+		false:
+			$AnimationPlayer.play("optionsIn")
+			are_options_on_screen = true
+			return
+		_:
+			print("something went wrong with the options animation")
+	
+
+
+func _on_ExitButton_button_down():
+	$ExitGamePopup.show()
+	print("Don't show this option on web export (MainMenu.gd)")
+
+
+func _on_Music_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"),value)
+
+
+func _on_Music_focus_entered():
+	if $Music.stream_paused:
+		$Music.stream_paused = false
+		return
+	$Music.play()
+
+
+func _on_Sound_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound"),value)
+
+
+func _on_ConfirmationDialog_confirmed():
+	get_tree().quit()
